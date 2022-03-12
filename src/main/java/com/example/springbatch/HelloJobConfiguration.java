d02905e9a062b696715213b3bc52b19a6a4b030a
cykei <kelle111@naver.com>
2022-03-12 16:06:20 +0900

    Initial commit

---
+++ src/main/java/com/example/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,60 @@
+package com.example.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor //생성자 DI를 생성자를 안만들어도 사용할 수 있게해주는 롬복 어노테이션
+@Configuration
+public class HelloJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1(){
+        return stepBuilderFactory.get("helloStep1")
+                .tasklet(new Tasklet() {
+                    // 디폴트 값이 무한 반복이다.
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("==============================");
+                        System.out.println(" >> hello spring batch!");
+                        System.out.println("==============================");
+
+                        return RepeatStatus.FINISHED; // 이렇게 리턴하거나 null을 리턴해야 한번 실행하면 끝난다.
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                .tasklet(new Tasklet() {
+                    // 디폴트 값이 무한 반복이다.
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("==============================");
+                        System.out.println(" >> hello job step 2!");
+                        System.out.println("==============================");
+
+                        return RepeatStatus.FINISHED; // 이렇게 리턴하거나 null을 리턴해야 한번 실행하면 끝난다.
+                    }
+                }).build();
+    }
+}
